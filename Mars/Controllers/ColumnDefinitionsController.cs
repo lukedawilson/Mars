a0460944d982375d5b7e3c90ed30fae7b58c82bd
Luke Wilson <luke.d.a.wilson@gmail.com>
2014-02-02 01:18:41 +0000

    Move class to own file.

---
+++ Mars/Controllers/ColumnDefinitionsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Mars.BusinessLogic;
+using Mars.Models;
+
+namespace Mars.Controllers
+{
+   public class ColumnDefinitionsController : ApiController
+   {
+      private readonly Repository _repository = new Repository();
+
+      // GET api/columnDefinitions
+      public IEnumerable<ColumnDefinition> Get(int? startRow = null, int? endRow = null)
+      {
+         return _repository.GetColumnDefinitions();
+      }
+   }
+}
--- Mars/Controllers/ValuesController.cs
+++ Mars/Controllers/ValuesController.cs
@@ -1,21 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using Mars.BusinessLogic;
-using Mars.Models;
 
 namespace Mars.Controllers
 {
-   public class ColumnDefinitionsController : ApiController
-   {
-      private readonly Repository _repository = new Repository();
-
-      // GET api/columnDefinitions
-      public IEnumerable<ColumnDefinition> Get(int? startRow = null, int? endRow = null)
-      {
-         return _repository.GetColumnDefinitions();
-      }
-   }
-
    public class ValuesController : ApiController
    {
       private readonly Repository _repository = new Repository();
